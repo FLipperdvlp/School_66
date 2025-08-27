7e90938bd2fc8c7e7b2d6b6c8b032032ebe7c601
Gleb Renkas <102474465+FLipperdvlp@users.noreply.github.com>
2025-08-27 22:29:31 +0300

    Update 3
    Add Razor page

---
+++ School_66/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace School_66.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- School_66/Program.cs
+++ School_66/Program.cs
@@ -3,13 +3,18 @@ using School_66.DataBase;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services.AddControllersWithViews();
     builder.Services.AddDbContext<AppDbContext>(options =>
-        options.UseSqlite("Data source = School_66.db"));
+        options.UseSqlite("Data source=School_66.db"));
     
 }
 var app = builder.Build();
 {
-    app.UseDefaultFiles();
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    
+    
     app.UseStaticFiles();
     app.Run();
 }

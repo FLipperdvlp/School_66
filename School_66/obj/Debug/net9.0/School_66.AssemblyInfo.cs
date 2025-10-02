e9dedb467729da81626d2a6778f89eaf678e5dca
Gleb Renkas <102474465+FLipperdvlp@users.noreply.github.com>
2025-10-02 21:11:33 +0200

    Update AssemblyInfo with new informational version and cache hash

--- School_66/obj/Debug/net9.0/School_66.AssemblyInfo.cs
+++ School_66/obj/Debug/net9.0/School_66.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School_66")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd10773c5d92e04f11b3755f3b9923c02656771c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6015b8fc3765ca92670942f9c769e5edfbd0c076")]
 [assembly: System.Reflection.AssemblyProductAttribute("School_66")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School_66")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

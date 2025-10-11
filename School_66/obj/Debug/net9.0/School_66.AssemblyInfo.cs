a9634c40617d3b63f8d00bcd7e827a076c311d3e
Gleb Renkas <102474465+FLipperdvlp@users.noreply.github.com>
2025-10-11 23:05:58 +0200

    Update 11

--- School_66/obj/Debug/net9.0/School_66.AssemblyInfo.cs
+++ School_66/obj/Debug/net9.0/School_66.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School_66")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c76797dbbf8c0501f10ce1d17c3b9813af7dc55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afaff922604117f7aaa15d4c21e75595fb93bc91")]
 [assembly: System.Reflection.AssemblyProductAttribute("School_66")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School_66")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

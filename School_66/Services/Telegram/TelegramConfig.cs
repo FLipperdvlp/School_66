5164608a7aec0282a77791d6b0a4404a3cd3499b
Gleb Renkas <102474465+FLipperdvlp@users.noreply.github.com>
2025-10-25 17:56:02 +0200

    Update .gitignore to include appsettings.json; initialize Token and AdminChatId in TelegramConfig

--- School_66/Services/Telegram/TelegramConfig.cs
+++ School_66/Services/Telegram/TelegramConfig.cs
@@ -1,5 +1,5 @@
 public class TelegramConfig
 {
-    public string Token { get; set; }
-    public string AdminChatId { get; set; }
+    public string Token { get; set; } = string.Empty;
+    public string AdminChatId { get; set; } = string.Empty;
 }
